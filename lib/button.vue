c84e0aa19e12c15dd249531dbf5a7a1367755c74
user.name <1165893368@qq.com>
2019-10-27 16:52:38 +0800

    edit:修改页面index导出

---
+++ lib/button.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  21312313213313
+  {{affter}}
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'button',
+  props:{
+      affter:String
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-const components = require.context('../lib', true, /[\w-]+.vue$/);
-components.keys().forEach(item => {
-var file = components(item);
-Vue.component('af-' + file.default.name, file.default)
-});
