24a263cfbe323acd280a7e48dcdea049746e810a
user.name <1165893368@qq.com>
2019-10-28 22:52:34 +0800

    add:markdown文档更新，button按钮组件开发

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # af-element
 UI-开源组件库
+# 安装
+
+```
+npm i af-elements
+```
+# 在main.js引入
+```
+import afComponents from 'af-element'
+Vue.use(afComponents)
+```
+# 使用示例
+```
+
+<af-button ><af-button/>
+```
+
+# 按钮
+
+参数|说明|类型|可选值|默认值
+--|:--:|--:|--:|--:
+size|按钮size|String|mini/small/medium|mini
+type|按钮类型|String|primary/success/info/warning/danger|无类型
+@click|点击事件|-|-|-
+
+
+slot
+名称|说明
+--:|--:
+-|自定义内容
+
+# 结语
+持续更新中
--- lib/button.vue
+++ lib/button.vue
@@ -1,22 +1,112 @@
 <template>
-<div>
-  21312313213313
-  {{affter}}
-</div>
+  <div>
+    <button
+    :class="['af-button',afColor,afSize]" 
+    @click="handleClick"
+    :type="type"
+    >
+    <slot></slot>
+    </button>
+  </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'button',
   props:{
-      affter:String
+      type:{
+        type:String,
+        default:'none'
+      },
+      size:{
+          type:String,
+          default:'mini'
+      }
+     
+  },
+  computed:{
+    afColor(){
+      const CLASS = this.type === 'primary'?'af-button-primary' : 
+                    this.type==='success' ? 'af-button-success' :
+                    this.type === 'info' ? 'af-button-info':
+                    this.type === 'warning'? 'af-button-warning' :
+                    this.type === 'danger'? 'af-button-danger' :
+                     ''
+      return CLASS                    
+    },
+    afSize(){
+      const SIZE = this.size === 'mini' ? 'af-size-mini' :
+                  this.size === 'small ' ? 'af-size-small ' : ''
+      return SIZE            
+    }
+
+    
+  },
+  methods:{
+    handleClick(data){
+      this.$emit('click',data)
+    }
   }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+  .af-button{
+    display: inline-block;
+      line-height: 1;
+      white-space: nowrap;
+      cursor: pointer;
+      background: #fff;
+      border: 1px solid #dcdfe6;
+      color: #606266;
+      -webkit-appearance: none;
+      text-align: center;
+      box-sizing: border-box;
+      outline: none;
+      margin: 0;
+      transition: .1s;
+      font-weight: 500;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+      padding: 12px 20px;
+      font-size: 14px;
+      border-radius: 4px;
+      }
+  .af-button-primary{
+    color: #fff;
+    background-color: #409eff;
+    border-color: #409eff;
+  }
+  .af-button-success{
+     color: #fff;
+    background-color: #67c23a;
+    border-color: #67c23a;
+  }
+    .af-button-info{
+     color: #fff;
+    background-color: #909399;
+    border-color: #909399;
+  }
+    .af-button-warning{
+     color: #fff;
+    background-color: #e6a23c;
+    border-color: #e6a23c;
+  }
+    .af-button-danger{
+     color: #fff;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+  }
+  .af-size-mini{
+    padding: 7px 15px;
+    font-size: 12px;
+    border-radius: 3px;
+  }
+  .af-size-small{
+    padding: 9px 15px;
+    font-size: 12px;
+    border-radius: 3px;
+  }
 </style>
